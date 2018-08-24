9bed6de788a3baec1da047d984643e1c7961088d
YuKai <>
2018-08-24 05:04:32 -0700

    insert title
    insert title

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -6,6 +6,7 @@ export class Home extends React.Component {
     render() {
         return(
             <div className="container">
+                <h1>Welcome</h1>
                 <ul className="inline-align">
                     <li><Link to={'/passenger'}><img className="menu-img imgResponsive" src="https://www.passengerterminaltoday.com/wp-content/uploads/2018/02/rsz_ptx2018_colour_dates.jpg" alt="passenger" /></Link></li>
                     <li><Link to={'/driver'}><img className="menu-img imgResponsive" src="http://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/761/2018/06/15090126/DriverServicesLogo.jpg" alt="driver" /></Link></li>
